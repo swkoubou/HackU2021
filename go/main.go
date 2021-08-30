acd3c05cb72651177a1daac1e0587d789542948e
okapon1210 <okapon1210@gmail.com>
2021-08-31 01:18:01 +0900

    routinggroupを作成した

--- go/main.go
+++ go/main.go
@@ -11,5 +11,44 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "%v\n", "hogehoge")
 	})
+
+	question := r.Group("question")
+	{
+		question.GET("", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+
+		question.POST("", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+
+		question.PUT("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+
+		question.DELETE("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+	}
+
+	collection := r.Group("collection")
+	{
+		collection.GET("", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+
+		collection.POST("", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+
+		collection.PUT("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+
+		collection.DELETE("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "%v\n", c.Request.Method)
+		})
+	}
+
 	r.Run()
 }
