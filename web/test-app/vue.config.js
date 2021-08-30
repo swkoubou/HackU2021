0509905e0407e4f2a3ff4293e9eeccc96fb83713
x0y14 <ya01ma08@gmail.com>
2021-08-30 12:12:27 +0900

    Revert "テストアプリをホスト、またはコンテナ内で起動するためにDockerfileと、VueJs2の設定ファイルに変更を加えました。"
    This reverts commit 730e0fdba9f850ec16425c7103a8fea73c416c3d.

--- web/test-app/vue.config.js
+++
@@ -1,7 +0,0 @@
-// 参考: https://dev.classmethod.jp/articles/change-vue-project-devserver-port/
-module.exports = {
-    devServer: {
-        port: 9090,
-        // disableHostCheck: true,
-    },
-};
