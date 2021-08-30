8ee3dfa862c26ce1254f06df88a935750269f70e
x0y14 <ya01ma08@gmail.com>
2021-08-30 12:35:20 +0900

    フロントエンド用のwebフォルダを作成し、VueJS2のdockerテスト用のアプリを作成しました。

---
+++ web/test-app/vue.config.js
@@ -0,0 +1,7 @@
+// 参考: https://dev.classmethod.jp/articles/change-vue-project-devserver-port/
+module.exports = {
+    devServer: {
+        port: 9090,
+        // disableHostCheck: true,
+    },
+};
