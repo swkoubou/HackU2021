c0dea76161d598595a7d625dd43074a0c52e8a67
okapon1210 <okapon1210@gmail.com>
2021-09-01 19:59:23 +0900

    パラメータを受け取って各構造体を生成する関数を作成した

--- go/question/question.go
+++ go/question/question.go
@@ -16,3 +16,34 @@ type Question struct {
 	Values       []string        `json:"values"`
 	Answers      []string        `json:"answers"`
 }
+
+type QuestionParam struct {
+	UserID       string
+	QuestionTag  []string
+	QuestionType string
+	QuestionBody string
+	Values       []string
+	Answers      []string
+}
+
+func (q *Question) ID() string {
+	return q.QuestionID.String()
+}
+
+func NewQuestion(param QuestionParam) (Question, error) {
+	q := Question{}
+
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		return q, err
+	}
+
+	q.QuestionID = uuid
+	q.Auther = account.Account{} // DBからもってくる
+	q.QuestionTag = param.QuestionTag
+	q.QuestionBody = param.QuestionBody
+	q.Values = param.Values
+	q.Answers = param.Answers
+
+	return q, nil
+}
