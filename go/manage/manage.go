e3a7c48cd7f009ccd60dec82365f2727b056e5f0
Orion042 <s1921062@cco.kanagawa-it.ac.jp>
2021-09-03 01:58:51 +0900

    レビューの修正しました．

--- go/manage/manage.go
+++ go/manage/manage.go
@@ -19,7 +19,6 @@ type dsn struct {
 }
 
 func NewDBConnection() (*sql.DB, error) {
-
 	dsn := &dsn{
 		user:     "root",
 		pass:     os.Getenv("MYSQL_ROOT_PASSWORD"),
@@ -29,21 +28,18 @@ func NewDBConnection() (*sql.DB, error) {
 	}
 
 	return sql.Open("mysql", dsn.user+":"+dsn.pass+"@"+dsn.protocol+"("+dsn.address+")/"+dsn.dbName)
-
-	// 下のはローカル環境で行った時に使用した，データベースである．
-	// データベースの内容はinit.sqlを使用
-
-	//return sql.Open("mysql", "root@/MYSQL_DATABASE")
 }
 
 func GetQuestion(questionID string) (question.Question, error) {
+
+	var user question.Question
 
 	// データベースへのアクセス
 	db, err := NewDBConnection()
 
 	// エラー処理
 	if err != nil {
-		panic(err.Error())
+		return user, err
 	}
 
 	// returnされた後に実行され，DBとの接続を切る
@@ -56,19 +52,17 @@ func GetQuestion(questionID string) (question.Question, error) {
 
 	// エラー処理
 	if err != nil {
-		panic(err.Error())
+		return user, err
 	}
 
 	var uuid_tmp1, uuid_tmp2 string
 
 	var tmp string
 
-	var user question.Question
-
 	// ここからデータベースでの処理
 	for rows1.Next() {
 		if err := rows1.Scan(&uuid_tmp1, &tmp, &uuid_tmp2, &user.QuestionType, &user.CreateTime, &user.UpdateTime, &user.QuestionBody); err != nil {
-			panic(err.Error())
+			return user, err
 		}
 		user.QuestionID, _ = uuid.Parse(uuid_tmp1)
 		user.Auther.UserID, _ = uuid.Parse(uuid_tmp2)
@@ -77,21 +71,21 @@ func GetQuestion(questionID string) (question.Question, error) {
 	rows2, err := db.Query("SELECT * FROM user WHERE user_id = ?", uuid_tmp2)
 
 	if err != nil {
-		panic(err.Error())
+		return user, err
 	}
 
 	var name_tmp string
 
 	for rows2.Next() {
 		if err := rows2.Scan(&name_tmp, &user.Auther.UserName); err != nil {
-			panic(err.Error())
+			return user, err
 		}
 	}
 
 	rows3, err := db.Query("SELECT * FROM question_tag_map WHERE question_id = ?", uuid_tmp1)
 
 	if err != nil {
-		panic(err.Error())
+		return user, err
 	}
 
 	var questionid_tmp, array_tmp string
@@ -100,17 +94,17 @@ func GetQuestion(questionID string) (question.Question, error) {
 
 	for rows3.Next() {
 		if err := rows3.Scan(&questionid_tmp, &tag_int); err != nil {
-			panic(err.Error())
+			return user, err
 		}
 
 		rows4, err := db.Query("SELECT * FROM question_tag WHERE question_tag_id = ?", tag_int)
 		if err != nil {
-			panic(err.Error())
+			return user, err
 		}
 
 		for rows4.Next() {
 			if err := rows4.Scan(&tag_int, &array_tmp); err != nil {
-				panic(err.Error())
+				return user, err
 			}
 			user.QuestionTag = append(user.QuestionTag, array_tmp)
 		}
@@ -119,14 +113,14 @@ func GetQuestion(questionID string) (question.Question, error) {
 	rows5, err := db.Query("SELECT * FROM question_value_map WHERE question_id = ?", uuid_tmp1)
 
 	if err != nil {
-		panic(err.Error())
+		return user, err
 	}
 
 	var int_tmp int
 
 	for rows5.Next() {
 		if err := rows5.Scan(&questionid_tmp, &int_tmp, &array_tmp); err != nil {
-			panic(err.Error())
+			return user, err
 		}
 		user.Values = append(user.Values, array_tmp)
 	}
@@ -134,27 +128,24 @@ func GetQuestion(questionID string) (question.Question, error) {
 	rows6, err := db.Query("SELECT * FROM question_answer_map WHERE question_id = ?", uuid_tmp1)
 
 	if err != nil {
-		panic(err.Error())
+		return user, err
 	}
 
 	for rows6.Next() {
 		if err := rows6.Scan(&questionid_tmp, &int_tmp, &array_tmp); err != nil {
-			panic(err.Error())
+			return user, err
 		}
 		user.Answers = append(user.Answers, array_tmp)
 	}
 
-	// 出力確認用
-	//fmt.Println(user)
+	// uuidのnil
+	var u uuid.NullUUID
 
-	// 文字列での比較を行うため
-	num_nil := "00000000-0000-0000-0000-000000000000"
-
-	if uuid_tmp1 == num_nil || uuid_tmp2 == num_nil || len(user.Auther.UserName) == 0 || len(user.QuestionTag) == 0 || len(user.QuestionType) == 0 || len(user.CreateTime) == 0 || len(user.UpdateTime) == 0 || len(user.QuestionBody) == 0 || len(user.Values) == 0 || len(user.Answers) == 0 {
+	if user.QuestionID == u.UUID || user.Auther.UserID == u.UUID || len(user.Auther.UserName) == 0 || len(user.QuestionTag) == 0 || len(user.QuestionType) == 0 || len(user.CreateTime) == 0 || len(user.UpdateTime) == 0 || len(user.QuestionBody) == 0 || len(user.Values) == 0 || len(user.Answers) == 0 {
 		user2 := question.Question{}
 		var isEmpty error
 
-		// エラーになる場合は，戻り値としてerrorをtrueとすることで，uuidによる00初期化を防止
+		// エラーになる場合は，戻り値としてerrorをtrueとすることで，uuidによる00初期化を防止．．あしあし
 		isEmpty = errors.New("true")
 		return user2, isEmpty
 	}
