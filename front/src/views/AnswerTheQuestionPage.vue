dad813aa1d911540f949a33ba3d260d7d4887e82
x0y14 <ya01ma08@gmail.com>
2021-09-03 14:56:58 +0900

    回答ページを追加

---
+++ front/src/views/AnswerTheQuestionPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{ this.sampleQuestionData }}</div>
+</template>
+
+<script>
+export default {
+    name: "AnswerTheQuestionPage",
+    data() {
+        return {
+            sampleQuestionDatas: require('@/testdata/question.json')
+        }
+    },
+    created() {
+        let samples = [this.sampleQuestionDatas.q_4taku, this.sampleQuestionDatas.q_anaume, this.sampleQuestionDatas.q_collection]
+        this.sampleQuestionData = samples[Math.floor(Math.random() * samples.length)];
+    }
+}
+</script>
