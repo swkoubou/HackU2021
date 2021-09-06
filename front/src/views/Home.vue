7f7a23aa4cf215c39858d920329e9c601733a8b2
nanagami1369 <nanagami1369@gmail.com>
2021-09-06 10:47:00 +0900

    ホーム画面を作成

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,11 +1,58 @@
 <template>
-  <div class="home-page">
-
-  </div>
+  <main class="home-page">
+    <PostList title="最新の問題" :postDataList="newQuestionPosts" />
+    <PostList title="最新の問題集" :postDataList="newQuestionPosts" />
+    <PostList
+      title="みんなが挑戦している問題"
+      :postDataList="newQuestionPosts"
+    />
+    <PostList
+      title="みんなが挑戦している問題集"
+      :postDataList="newQuestionPosts"
+    />
+  </main>
 </template>
 
 <script>
+import PostList from '@/components/QuestionAndCollectionPostList'
+
 export default {
   name: 'Home',
+  components: {
+    PostList,
+  },
+  data() {
+    return {
+      newQuestionPosts: [],
+    }
+  },
+  created() {
+    const questionDataSamples = require('@/testdata/question.json')
+    questionDataSamples.multipleProblemSamples
+    // 新しいデータを挿入
+    // Todo 後でAPIに置き換え
+    this.newQuestionPosts = []
+    this.newQuestionPosts = questionDataSamples.multipleProblemSamples
+  },
 }
 </script>
+
+<style scoped>
+.new-question-post {
+  text-align: start;
+  margin: 20px 10px;
+}
+.new-question-post-line {
+  display: flex;
+  gap: 0px 20px;
+  overflow-x: scroll;
+}
+.new-question-post-title {
+  padding: 10px 30px;
+  font-size: 1em;
+  font-weight: bold;
+  border: solid #2c3e50 2px;
+  border-radius: 50px;
+  display: inline-block;
+}
+</style>
