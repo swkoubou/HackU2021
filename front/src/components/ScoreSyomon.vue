9193e9d16b1b8086d078af00e63c75abcceff559
x0y14 <ya01ma08@gmail.com>
2021-09-06 13:58:20 +0900

    スコアページに問題のスコア、正誤を表示できるように

---
+++ front/src/components/ScoreSyomon.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="score-syomon">
+    <h4 class="score-syomon-no">問{{ SyomonNo }}</h4>
+    <div class="score-compare-answers">
+      <div class="score-compare-answers-user">
+        あなたの回答 : {{ (UserAnswer == null) ? "未回答" : UserAnswer }}
+      </div>
+      <div class="score-compare-answers-question">
+        問題の答え : {{ QuestionAnswer }}
+      </div>
+    </div>
+    <div class="isCorrentAnswer-true" v-if="isCorrentAnswer == true">正解</div>
+    <div class="isCorrentAnswer-false" v-else>不正解</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScoreSyomon',
+  props: {
+    SyomonNo: {
+      type: Number,
+      required: true,
+    },
+    UserAnswer: {
+      type: String,
+      required: true,
+    },
+    QuestionAnswer: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    isCorrentAnswer: function () {
+      return this.UserAnswer === this.QuestionAnswer
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+.score-syomon {
+    justify-content: space-evenly;
+    max-width: 500px;
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+}
+
+.score-compare-answers {
+    text-align: start;
+}
+</style>
--- front/src/views/ScorePage.vue
+++ front/src/views/ScorePage.vue
@@ -1,12 +1,69 @@
 <template>
   <div>
-    <h3>{{ answerData }}</h3>
+    <h2>スコア</h2>
+    <div class="daimons">
+
+    <div class="daimon"
+      v-for="(daimon, daimonIndex) in answerData"
+      :key="`daimon-${daimonIndex}`"
+    >
+      <div class="question-score-card">
+        <h3 class="daimon-no">大問{{ daimonIndex + 1 }}</h3>
+        <div class="syomons">
+        <div
+        class="syomon"
+          v-for="(syomon, syomonIndex) in daimon"
+          :key="`syomon-${syomonIndex}`"
+        >
+          <ScoreSyomon
+            :SyomonNo="syomonIndex + 1"
+            :UserAnswer="syomon.userAnswer"
+            :QuestionAnswer="syomon.questionAnswer"
+          />
+        </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import ScoreSyomon from '@/components/ScoreSyomon.vue'
+
 export default {
   name: 'ScorePage',
-  props: ['answerData'],
+  props: {
+    answerData: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    ScoreSyomon,
+  },
 }
 </script>
+
+<style scoped>
+.syomons {
+    align-items: center;
+    justify-content: center;
+}
+.syomon {
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 5px;
+}
+
+/* .daimons { */
+
+/* } */
+
+.daimon {
+    background: chocolate;
+    align-items: center;
+    justify-content: center;
+}
+</style>
