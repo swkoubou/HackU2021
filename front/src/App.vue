d6636e0f2cd88c7a681d152b7c0a80b91d423bc1
nanagami1369 <nanagami1369@gmail.com>
2021-09-07 15:33:04 +0900

    CSSを画面全体に反映できるようにした

--- front/src/App.vue
+++ front/src/App.vue
@@ -31,12 +31,26 @@ export default {
 </script>
 
 <style>
+html {
+  height: 100%;
+  width: 100%;
+  margin: 0px;
+}
+
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  width: 100%;
 }
 
 #nav {
