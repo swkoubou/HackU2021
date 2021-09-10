7622c303a5fcffb16260c4e3352ff3371041699e
nanagami1369 <nanagami1369@gmail.com>
2021-09-10 17:41:31 +0900

    横幅が短い時に20px、横のマージンを取るように変更

--- front/src/views/ScorePage.vue
+++ front/src/views/ScorePage.vue
@@ -47,16 +47,19 @@ export default {
 </script>
 
 <style scoped>
-.syomons {
-  align-items: center;
-  justify-content: center;
-}
 .syomon {
   background: rgb(236, 236, 236);
-  width: 80%;
   max-width: 800px;
+  margin: 5px 0px;
   margin-left: auto;
   margin-right: auto;
-  margin-bottom: 5px;
+}
+
+/* 横幅が少ない時の最低マージン */
+@media (max-width: 840px) {
+  .syomon {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 }
 </style>
