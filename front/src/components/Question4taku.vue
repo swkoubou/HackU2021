d9bb567060a7a5ab0718ce07310053c2b07e22ba
x0y14 <ya01ma08@gmail.com>
2021-09-11 03:27:59 +0900

    Revert "遷移を行う前に、選択を初期化するように変更しました。"
    This reverts commit 797bf909dc35bf5d4e4b23770665c2f17427435e.

--- front/src/components/Question4taku.vue
+++ front/src/components/Question4taku.vue
@@ -45,9 +45,6 @@ export default {
       selectingQuestionIndex: null,
     }
   },
-  created(){
-    this.selectingQuestionIndex = null;
-  },
   methods: {
     setSelecting: function (index) {
       if (this.selectingQuestionIndex != index) {
@@ -66,7 +63,6 @@ export default {
         this.$parent.goScorePageAndCheckAnswers(answersData)
       } else {
         this.$parent.storeUserAnswers(answersData)
-        this.selectingQuestionIndex = null
         this.$parent.goNextQuestion()
       }
     },
