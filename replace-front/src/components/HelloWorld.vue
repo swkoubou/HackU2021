bb865560ff29d659c126682301255d48046ecc06
nanagami1369 <nanagami1369@gmail.com>
2021-10-17 11:50:13 +0900

    tailwind cssを追加

--- replace-front/src/components/HelloWorld.vue
+++ replace-front/src/components/HelloWorld.vue
@@ -14,42 +14,42 @@ const increment = () => store.dispatch('increment')
 
   <p>
     Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    <a href="https://code.visualstudio.com/" target="_blank" class="a-tag"
+      >VSCode</a
+    >
     +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+      class="a-tag"
+      >Volar</a
+    >
   </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+  <p>
+    See
+    <code class="code">README.md</code>
+    for more information.
+  </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+      class="a-tag"
+    >
       Vite Docs
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    <a href="https://v3.vuejs.org/" target="_blank" class="a-tag">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="increment">count is: {{ count }}</button>
+  <button type="button" @click="increment" class="button">
+    count is: {{ count }}
+  </button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code class="code">components/HelloWorld.vue</code>
+    to test hot module replacement.
   </p>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
--- replace-front/src/main.ts
+++ replace-front/src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { key, store } from './store'
+import './index.css'
 createApp(App).use(store, key).use(router).mount('#app')
