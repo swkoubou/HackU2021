706a9ba83830922bc64b317c516f0bd08b52bb64
x0y14 <ya01ma08@gmail.com>
2021-10-29 01:21:42 +0900

    モデル、定義を追加

---
+++ replace-front/src/models/collection_model.ts
@@ -0,0 +1,14 @@
+import { User } from "./user_model";
+import { Question } from "./question_model";
+
+export interface Collection {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    author: User;
+
+    title: string;
+    description: string;
+    questions: Question[];
+    color: string;
+}
