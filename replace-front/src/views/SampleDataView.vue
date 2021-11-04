cd8a13c1c4aee074ecdd0b110ea2e2779c74f08b
x0y14 <ya01ma08@gmail.com>
2021-11-04 19:24:12 +0900

    サンプルに関して、よくわからないので、todoにした

---
+++ replace-front/src/views/SampleDataView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+
+import { computed } from 'vue'
+</script>
+
+<template>
+  <div class="sample">
+    <h1>hello, todo</h1>
+  </div>
+</template>
