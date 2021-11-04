be2c324b7f410665342ef2d2a52e63427d979e8b
x0y14 <ya01ma08@gmail.com>
2021-11-04 19:50:29 +0900

    サンプルページに、名前を表示できるように

--- replace-front/src/views/SampleDataView.vue
+++ replace-front/src/views/SampleDataView.vue
@@ -2,10 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 
 import { computed } from 'vue'
+import { CreateUserRepository } from '../infrastructures/offline_user_repository_impl'
+
+const userRepo = CreateUserRepository()
 </script>
 
 <template>
   <div class="sample">
-    <h1>hello, todo</h1>
+    <h1>hello, {{ userRepo.Get('').displayName }}</h1>
   </div>
 </template>
