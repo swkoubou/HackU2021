4706f4e184926c3bca84baa47d263ab77481c97a
x0y14 <ya01ma08@gmail.com>
2021-11-04 19:54:05 +0900

    名前をcomputedを使用して呼び出してみる

--- replace-front/src/views/SampleDataView.vue
+++ replace-front/src/views/SampleDataView.vue
@@ -5,10 +5,11 @@ import { computed } from 'vue'
 import { CreateUserRepository } from '../infrastructures/offline_user_repository_impl'
 
 const userRepo = CreateUserRepository()
+const displayName = computed(() => userRepo.Get('').displayName)
 </script>
 
 <template>
   <div class="sample">
-    <h1>hello, {{ userRepo.Get('').displayName }}</h1>
+    <h1>hello, {{ displayName }}</h1>
   </div>
 </template>
