7af3208e337e87edd9ae7d7a112785f1cb61b96d
x0y14 <ya01ma08@gmail.com>
2021-11-04 13:21:52 +0900

    エクスポートしました。 エラーをエクステンドするようにしました。

--- replace-front/src/Infrastructures/error_infrastructures.ts
+++ replace-front/src/Infrastructures/error_infrastructures.ts
@@ -1,38 +1,36 @@
-// 参考: http://exhikkii.com/2020/11/01/typescript-カスタムエラーの作成の仕方/
+// 参考:
+// http://exhikkii.com/2020/11/01/typescript-カスタムエラーの作成の仕方/
+// https://stackoverflow.com/questions/41102060/typescript-extending-error-class
+// https://qiita.com/suema0331/items/374c0757aa00b37d98bd
 
 // 元になるエラー
-class OfflineDBError implements Error {
-  public name = 'OfflineDBError'
-  constructor(public message: string) {
-    if (typeof console !== 'undefined') {
-      console.log(`name: ${this.name}, message: ${this.message}`)
-    }
-  }
-  toString() {
-    return `${this.name} ${this.message}`
+export abstract class OfflineDBError extends Error {
+  public abstract override readonly name: string
+  constructor(message: string) {
+    super(message)
   }
 }
 
 // ユーザーが見つからない
-class OfflineDBUserNotFoundError extends OfflineDBError {
+export class OfflineDBUserNotFoundError extends OfflineDBError {
   public name = 'OfflineDBUserNotFoundError'
-  constructor(public id: string) {
+  constructor(id: string) {
     super(`userId not found: ${id}`)
   }
 }
 
 // クエスチョンが見つからない
-class OfflineDBQuestionNotFoundError extends OfflineDBError {
+export class OfflineDBQuestionNotFoundError extends OfflineDBError {
   public name = 'OfflineDBQuestionNotFoundError'
-  constructor(public id: string) {
+  constructor(id: string) {
     super(`questionId not found: ${id}`)
   }
 }
 
 // コレクションが見つからない
-class OfflineDBCollectionNotFoundError extends OfflineDBError {
+export class OfflineDBCollectionNotFoundError extends OfflineDBError {
   public name = 'OfflineDBCollectionNotFoundError'
-  constructor(public id: string) {
+  constructor(id: string) {
     super(`collectionId not found: ${id}`)
   }
 }
